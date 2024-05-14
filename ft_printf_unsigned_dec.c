3256ed24fb09b45e24b00af170ca448da5dc18be
Yuga Ehara <yehara@c2r8s5.42tokyo.jp>
2024-05-14 17:11:06 +0900

    correction unsigned print

--- ft_printf_unsigned_dec.c
+++ ft_printf_unsigned_dec.c
@@ -6,22 +6,19 @@
 /*   By: yehara <yehara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 22:26:47 by yehara            #+#    #+#             */
-/*   Updated: 2024/05/13 23:40:43 by yehara           ###   ########.fr       */
+/*   Updated: 2024/05/14 17:06:06 by yehara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#define BINARY 32
 
-static int	decimal_to_binary(int dec)
+static char *decimal_to_binary(int dec, char *result, int i)
 {
-	char	*result;
-	int i;
-    
-    i = 8;
 	if (0 < dec)
-		decimal_to_binary(dec / 10);
-	result[i--] = ft_itoa(dec % 2);
-	return (ft_atoi(result));
+		decimal_to_binary(dec / 2, result, i++);
+	result[i++] = (dec % 2) + '0';
+	return (result);
 }
 
 static int	binary_to_decimal(int bin)
@@ -44,11 +41,20 @@ static int	binary_to_decimal(int bin)
 
 int	ft_printf_unsigned_dec(unsigned int n)
 {
-	int binary;
-    char *result;
-	binary = decimal_to_binary(n);
-    printf("%u\n", binary);
-	binary = ~(binary + 1);
+	char *bin;
+	unsigned long long binary;
+	char *result;
+	int i;
+
+	i = 0;
+	if (n < 0)
+		n *= -1;
+	bin = (char *)ft_calloc(BINARY, sizeof(char));
+	if (!bin)	
+		return (0);
+	binary = ft_atoi(decimal_to_binary(n, bin, i));
+	printf("%llu", binary);
+	binary = ~(binary) + 1;
     result = ft_itoa(binary_to_decimal(binary));
 	ft_printf_str(result);
 	return (ft_strlen(result));
